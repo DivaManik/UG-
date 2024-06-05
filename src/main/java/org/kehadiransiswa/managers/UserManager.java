aba20df2a4195208ad7f08a2f41e40c64e60e4a6
Diva Filemon Manik <158981691+DivaManik@users.noreply.github.com>
2024-06-05 12:26:26 +0700

    Update UserManager.java

--- src/main/java/org/kehadiransiswa/managers/UserManager.java
+++ src/main/java/org/kehadiransiswa/managers/UserManager.java
@@ -1,89 +1,34 @@
-package org.kehadiransiswa.managers;
+package com.ug9.eTransactionProject;
 
-import org.kehadiransiswa.data.User;
+public class MobileWallet extends DigitalPayment {
+    private String noHP;
+    protected long feeTransferBank;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public class UserManager {
-
-    private Connection connection;
-
-    List<User> users;
-
-//    public static void main(String[] args) {
-//        UserManager usr = new UserManager();
-//        for (User user : usr.getAllUsers()) {
-//            System.out.println(user.getEmail());
-//        }
-//    }
-    public UserManager() {
-        connection = DBConnectionManager.getConnection();
-        users = new ArrayList<>();
+    public MobileWallet(String nama, long saldo, String noHp) {
+        super(nama, saldo);
+        this.noHP = noHp;
     }
 
-    public List<User>getAllUsers(){
-        List<User>listOfUser = new ArrayList<User>();
-
-        try {
-            Statement statement =connection.createStatement();
-            ResultSet rs =statement.executeQuery("SELECT * from users");
-            while (rs.next()){
-                int id =rs.getInt("id");
-                String username = rs.getString("username");
-                String password = rs.getString("password");
-                String email = rs.getString("email");
-                String role  = rs.getString("role");
-                listOfUser.add(new User(id, username, password, email, role));
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
-            System.exit(1);
+    @Override
+    public void transfer(DigitalPayment dp, long nominal) {
+        if (nominal <= 0) {
+            System.out.println("Nominal yang Anda input tidak valid!");
+            return;
         }
-        return listOfUser;
-    }
-
-    public boolean registerUser(String username, String password, String email, String role) {
-        User newUser = new User(users.size()+1, username, password, email, role);
-        if(users.isEmpty()){
-            users.add(newUser);
-            try {
-                PreparedStatement statement = connection.prepareStatement("INSERT INTO users(username,password,email,role) values (?,?,?,?)");
-                statement.setString(1,username);
-                statement.setString(2,password);
-                statement.setString(3,email);
-                statement.setString(4,role);
-                statement.executeUpdate();
-            }catch (SQLException e){
-                e.printStackTrace();
-                System.exit(1);
-            }
-            return true;
+        if (getSaldo() < nominal) {
+            System.out.println("Transfer gagal! Saldo Anda tidak mencukupi.");
+            return;
         }
-        for (User user : users) {
-            if (!user.getEmail().equalsIgnoreCase(newUser.getEmail())) {
-                users.add(newUser);
-                return true;
+        if (dp instanceof BRImo || dp instanceof BNImo) {
+            if (getSaldo() < nominal + feeTransferBank) {
+                System.out.println("Transfer gagal! Saldo Anda tidak mencukupi.");
+                return;
             }
+            setSaldo(getSaldo() - (nominal + feeTransferBank));
+        } else {
+            setSaldo(getSaldo() - nominal);
         }
-
-        return false;
-    }
-
-    public boolean updateProfile(int id, String username, String password, String email, String role) {
-        User newUser = new User(id, username, password, email, role);
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).getId() == newUser.getId()) {
-                users.set(i, newUser);
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    public List<User> getAllUser() {
-        return users;
+        dp.setSaldo(dp.getSaldo() + nominal);
+        printBuktiTransfer(dp, nominal);
     }
 }
